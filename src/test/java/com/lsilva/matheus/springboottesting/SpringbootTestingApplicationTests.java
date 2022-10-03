0d834eb044fb82fe93222c66a2685eac9916bff4
lsilvamatheus <lsilva.matheus@gmail.com>
2022-10-03 19:45:50 -0300

    Criando testHelloWorld

--- src/test/java/com/lsilva/matheus/springboottesting/SpringbootTestingApplicationTests.java
+++ src/test/java/com/lsilva/matheus/springboottesting/SpringbootTestingApplicationTests.java
@@ -1,13 +1,33 @@
 package com.lsilva.matheus.springboottesting;
 
+import com.lsilva.matheus.springboottesting.controller.HelloWorldController;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
 
-@SpringBootTest
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(controllers = HelloWorldController.class)
 class SpringbootTestingApplicationTests {
 
+	@Autowired
+	private MockMvc mockMvc;
+
+	@BeforeAll
+	public static void setup() {
+
+	}
+
 	@Test
-	void contextLoads() {
+	void testHelloWorld() throws Exception {
+		this.mockMvc
+				.perform(get("/hello"))
+				.andExpect(status().isOk())
+				.andExpect(content().string("Hello World"));
 	}
 
 }
